3aea67503165153d0bfde948d5b0f5fc02382a19
IagoWag <iagowagner12@gmail.com>
2022-07-09 19:37:58 -0300

    added navigation animation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+    CardStyleInterpolators,
+    createStackNavigator,
+    StackNavigationOptions,
+} from '@react-navigation/stack';
 import Internal from '../screens/Internal';
 import Home from '../screens/Home';
 
 const {Navigator, Screen} = createStackNavigator();
 
+const animatedNavigation: StackNavigationOptions = {
+    gestureDirection: 'horizontal',
+    gestureEnabled: true,
+    headerShown: false,
+    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+};
+
 const Routes: React.FC = () => {
     return (
-        <Navigator screenOptions={{headerShown: false}}>
+        <Navigator screenOptions={animatedNavigation}>
             <Screen options={{title: 'Home'}} name="Home" component={Home} />
             <Screen
                 options={{title: 'Internal'}}
